88a1602b4469dbecc531d57090aaed23f6bb6533
Simon W. <s.wallmann@kompsoft.de>
2026-07-15 20:28:39 +0200

    Add pre-menarche constants to feature/symptom-logging
    - Add PRE_MENARCHE state constants (STATE_PRE_MENARCHE, STATE_MENARCHE)
    - Add menarche-related attributes (ATTR_AWAITING_MENARCHE, etc.)
    - Add menarche services (set_menarche_mode, update_menarche_date, etc.)
    - Add pre-menarche body signs (pubic_hair, breast, etc.)
    - Add Tanner stages (stage_1 to stage_5)
    - Add service field constants for menarche
    - Add default age ranges for menarche
    - Bilingual support: English and German comments

--- custom_components/menstruation_gauge/const.py
+++ custom_components/menstruation_gauge/const.py
@@ -32,6 +32,12 @@ ATTR_PREGNANCY_START_DATE = "pregnancy_start_date"
 ATTR_WEEKS_PREGNANT = "weeks_pregnant"
 ATTR_DUE_DATE = "due_date"
 ATTR_PREGNANCY_DATA = "pregnancy_data"
+ATTR_AWAITING_MENARCHE = "awaiting_menarche"
+ATTR_ESTIMATED_MENARCHE_DATE = "estimated_menarche_date"
+ATTR_DAYS_UNTIL_MENARCHE = "days_until_menarche"
+ATTR_AGE_AT_TRACKING = "age_at_tracking"
+ATTR_FAMILY_MENARCHE_AGE = "family_menarche_age"
+ATTR_PRE_MENARCHE_DATA = "pre_menarche_data"
 
 SERVICE_ADD_CYCLE_START = "add_cycle_start"
 SERVICE_REMOVE_CYCLE_START = "remove_cycle_start"
@@ -45,6 +51,11 @@ SERVICE_REMOVE_SYMPTOM = "remove_symptom"
 SERVICE_GET_SYMPTOM = "get_symptom"
 SERVICE_SET_PREGNANCY_MODE = "set_pregnancy_mode"
 SERVICE_UPDATE_PREGNANCY_DATE = "update_pregnancy_date"
+SERVICE_SET_MENARCHE_MODE = "set_menarche_mode"
+SERVICE_UPDATE_MENARCHE_DATE = "update_menarche_date"
+SERVICE_GET_MENARCHE_INFO = "get_menarche_info"
+SERVICE_ADD_PRE_MENARCHE_SIGN = "add_pre_menarche_sign"
+SERVICE_REMOVE_PRE_MENARCHE_SIGN = "remove_pre_menarche_sign"
 
 SERVICE_FIELD_DATE = "date"
 SERVICE_FIELD_DATES = "dates"
@@ -58,6 +69,10 @@ SERVICE_FIELD_ENTITY_ID = "entity_id"
 SERVICE_FIELD_SYMPTOM_DATA = "symptom_data"
 SERVICE_FIELD_IS_PREGNANT = "is_pregnant"
 SERVICE_FIELD_PREGNANCY_START_DATE = "pregnancy_start_date"
+SERVICE_FIELD_ESTIMATED_MENARCHE_DATE = "estimated_menarche_date"
+SERVICE_FIELD_FAMILY_MENARCHE_AGE = "family_menarche_age"
+SERVICE_FIELD_PRE_MENARCHE_SIGN = "pre_menarche_sign"
+SERVICE_FIELD_TANNER_STAGE = "tanner_stage"
 
 SIGNAL_HISTORY_UPDATED = "menstruation_gauge_history_updated"
 
@@ -66,6 +81,8 @@ STATE_FERTILE = "fertile"
 STATE_PMS = "pms"
 STATE_NEUTRAL = "neutral"
 STATE_PREGNANT = "pregnant"
+STATE_PRE_MENARCHE = "pre_menarche"
+STATE_MENARCHE = "menarche"
 
 # Symptom field definitions
 SYMPTOM_BLEEDING_STRENGTH = "bleeding_strength"
@@ -85,3 +102,35 @@ SYMPTOM_OPTIONS = {
     SYMPTOM_HYGIENE: ["pad", "liner", "tampon", "cup", "period_underwear"],
     SYMPTOM_TEST: ["positive_ovulation", "negative_ovulation", "positive_pregnancy", "negative_pregnancy"],
 }
+
+# Pre-Menarche Body Signs - Tanner Stages
+PRE_MENARCHE_SIGN_PUBIC_HAIR = "pubic_hair_growth"
+PRE_MENARCHE_SIGN_BREAST = "breast_development"
+PRE_MENARCHE_SIGN_HEIGHT_SPURT = "height_spurt"
+PRE_MENARCHE_SIGN_MOOD = "mood_changes"
+PRE_MENARCHE_SIGN_ACNE = "acne"
+PRE_MENARCHE_SIGN_BODY_ODOR = "body_odor"
+PRE_MENARCHE_SIGN_DISCHARGE = "vaginal_discharge"
+
+# Tanner Stages (1-5) - Medical Standard
+TANNER_STAGE_1 = "stage_1"      # Pre-puberty / Vorpupertät
+TANNER_STAGE_2 = "stage_2"      # Early puberty / Frühe Pubertät
+TANNER_STAGE_3 = "stage_3"      # Mid puberty / Mittlere Pubertät
+TANNER_STAGE_4 = "stage_4"      # Late puberty / Späte Pubertät
+TANNER_STAGE_5 = "stage_5"      # Adult / Erwachsenenalter
+
+# Pre-Menarche Sign Options
+PRE_MENARCHE_SIGN_OPTIONS = {
+    PRE_MENARCHE_SIGN_PUBIC_HAIR: [TANNER_STAGE_1, TANNER_STAGE_2, TANNER_STAGE_3, TANNER_STAGE_4, TANNER_STAGE_5],
+    PRE_MENARCHE_SIGN_BREAST: [TANNER_STAGE_1, TANNER_STAGE_2, TANNER_STAGE_3, TANNER_STAGE_4, TANNER_STAGE_5],
+    PRE_MENARCHE_SIGN_HEIGHT_SPURT: ["none", "slight", "moderate", "significant"],
+    PRE_MENARCHE_SIGN_MOOD: ["stable", "mild_changes", "noticeable_changes", "significant_changes"],
+    PRE_MENARCHE_SIGN_ACNE: ["none", "slight", "moderate", "severe"],
+    PRE_MENARCHE_SIGN_BODY_ODOR: ["none", "slight", "moderate", "strong"],
+    PRE_MENARCHE_SIGN_DISCHARGE: ["none", "clear", "white", "clear_to_white"],
+}
+
+# Default age ranges for menarche (varies by population)
+DEFAULT_MENARCHE_AGE_MIN = 9
+DEFAULT_MENARCHE_AGE_MAX = 16
+DEFAULT_MENARCHE_AGE_TYPICAL = 12
